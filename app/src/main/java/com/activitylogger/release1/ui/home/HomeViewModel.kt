81f3d9090fb575728254ed2a87ef382dd5bf4df5
GoSooners345 <Boomersooner12345@gmail.com>
2022-03-21 17:27:21 -0400

    Added tags to db items for end user to categorize entries for faster searching

--- app/src/main/java/com/activitylogger/release1/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/activitylogger/release1/ui/home/HomeViewModel.kt
@@ -5,11 +5,12 @@ import com.activitylogger.release1.async.RecordsRepository
 import com.activitylogger.release1.data.Records
 import kotlinx.coroutines.DelicateCoroutinesApi
 
-class HomeViewModel() : ViewModel() {
-var recordsRepo: RecordsRepository? =null
-//Handles deleting records
-@DelicateCoroutinesApi
-fun deleteRecord(record: Records){
+class HomeViewModel : ViewModel() {
+    var recordsRepo: RecordsRepository? = null
+
+    //Handles deleting records
+    @DelicateCoroutinesApi
+    fun deleteRecord(record: Records) {
         recordsRepo!!.deleteRecord(record)
 
     }
